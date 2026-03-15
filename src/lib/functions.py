9f45fff3cb3d0a1b314490171f3825c83f8f84c5
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2026-03-15 22:32:08 +0100

    add(examples): start adding some example ideas

---
+++ src/lib/functions.py
@@ -0,0 +1,11 @@
+def linear_classic():
+    pass
+
+def linear_classic_noised():
+    pass
+
+def almost_linear():
+    pass
+
+def other_v1():
+    pass
---
+++ src/lib/gates.py
@@ -0,0 +1,14 @@
+def gates_and_2():
+    pass
+
+def gates_and_5():
+    pass
+
+def gates_or_5():
+    pass
+
+def gates_complex_v1():
+    pass
+
+def gates_complex_v2():
+    pass
