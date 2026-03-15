8325d55e0284129cd1590c5f4a4c067f8397833b
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2026-03-15 17:29:23 +0100

    add(flake&uv): init flake and uv project

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "flake for testing pytorch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      fpkgs = sys: nixpkgs.legacyPackages.${sys};
+      merge =
+        content:
+        builtins.listToAttrs (
+          map (sys: {
+            name = sys;
+            value = content sys;
+          }) systems
+        );
+    in
+    {
+      devShell = merge (
+        system:
+        let
+          pkgs = fpkgs system;
+          cclib = pkgs.stdenv.cc.cc.lib;
+        in
+        pkgs.mkShell {
+          shellHook = ''
+            export LD_LIBRARY_PATH=${cclib}/lib:$LD_LIBRARY_PATH
+
+            uv sync
+            source .venv/bin/activate
+          '';
+
+          buildInputs =
+            with pkgs;
+            [
+              nixfmt
+              python3
+              gcc
+              uv
+              cclib
+            ];
+        }
+      );
+    };
+}
